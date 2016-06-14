a602ba077fbacbeb0f7d0d3631f51cceeab0a8f4
caijinzheng <1007189142@qq.com>
2016-06-14 20:38:58 +0800

    改变geiBuildingInfo

--- php/getBuildingInfo.php
+++ php/getBuildingInfo.php
@@ -9,10 +9,20 @@ while ($row=mysql_fetch_array($result1)) {
 	$building_data["Floors"]=$row["Floors"];
 	$building_data["Attribute"]=$row["Attribute"];
 	$building_data["B_Type"]=$row["B_Type"];
+	$building_data["LocationPath"]=$row["LocationPath"];
 }
+#从Floor中获取BaseMap信息
+$basemap=array();
+$result4=mysql_query("SELECT * FROM Floor WHERE B_ID=$B_ID");
+while ($row=mysql_fetch_array($result4)) {
+	$New_arr=array();
+	$New_arr["BaseMap"]=$row["BaseMap"];
+	$basemap[$row['Floor']]=$New_arr;
+}
+$building_data["BaseMap"]=$basemap;
 $result2=mysql_query("SELECT * FROM room WHERE B_ID=$B_ID");
 $room_data=array();
-$R_ID=array();
+$facility_data=array();
 #获得该楼所有房间的信息
 while ($row=mysql_fetch_array($result2)) {
 	$New_arr=array();
@@ -22,30 +32,21 @@ while ($row=mysql_fetch_array($result2)) {
 	$New_arr["Y"]=$row["Y"];
 	$New_arr["R_Name"]=$row["R_Name"];
 	$New_arr["R_Area"]=$row["R_Area"];
+	$R_ID=$row["R_ID"];
+	$result5=mysql_query("SELECT * FROM facility WHERE R_ID=$R_ID");
+          while ($row1=mysql_fetch_array($result5)) {
+          	         $New_arr1=array();
+          	         $New_arr1["F_Name"]=$row1["F_Name"];
+          	         $New_arr1["F_Num"]=$row1["F_Num"];
+                   $facility_data[$row1['F_ID']]=$New_arr1;
+          }
+          $New_arr["facility"]=$facility_data;
 	$room_data[$row["R_ID"]]=$New_arr;
-           $i=0;
-           $R_ID[$i]=$row["R_ID"];
-           $i++;
 }
-#获得所有房间里的设施信息
-$room_facility_data=array();
-foreach ($R_ID as $value) {
-	$result3=mysql_query("SELECT * FROM facility WHERE R_ID=$value");
-	$facility_data=array();
-	while ($row=mysql_fetch_array($result3)) {
-		$New_arr=array();
-		$New_arr["F_Name"]=$row["F_Name"];
-		$New_arr["F_Num"]=$row["F_Num"];
-		$facility_data[$row["F_ID"]]=$New_arr;                                                                   
-	}
-	$room_facility_data[$value]=$facility_data;
 
-}
 $data=array();
 $data["building"]=$building_data;
 $data["room"]=$room_data;
-$data["facility"]=$room_facility_data;
-#var_dump($data);
 $json_data=json_encode($data);
 
 echo( $json_data);
