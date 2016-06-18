3dda12cc2e6a8fcac4627931d8624aa97ff6574e
caijinzheng <1007189142@qq.com>
2016-06-18 10:08:26 +0800

    修改modRoomFacility.php

--- php/modRoomFacility.php
+++ php/modRoomFacility.php
@@ -1,7 +1,8 @@
 <?php 
 include("DBconn.php");
 @$data=$_POST;
-$F_ID=$data[0]["F_ID"];
+//var_dump($data);
+$F_ID=$data["Facility"][0]["F_ID"];
 #首先根据F_ID获取到R_ID
 $result=mysql_query("SELECT * FROM facility WHERE F_ID=$F_ID");
 $R_ID=0;
@@ -12,9 +13,11 @@ while ($row=mysql_fetch_array($result)) {
 $result1=mysql_query("DELETE FROM facility WHERE R_ID=$R_ID");
 #将前台数据全部添加到数据库
 foreach ($data as $key => $value) {
-         
-	$result2=mysql_query("INSERT INTO facility(F_ID,F_Name,F_Num,R_ID) VALUES($value[F_ID],'$value[F_Name]',$value[F_Num],$R_ID)");
+         foreach ($value as $key => $value1) {
+         	$result2=mysql_query("INSERT INTO facility(F_ID,F_Name,F_Num,R_ID) VALUES($value1[F_ID],'$value1[F_Name]',$value1[F_Num],$R_ID)");
 }
+         }
+	
 
 //var_dump($data);
 echo("更新成功");
