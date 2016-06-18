86a57d27af1d149d4d0b972144d49979e5806526
caijinzheng <1007189142@qq.com>
2016-06-18 10:55:20 +0800

    删掉F_ID

--- php/modRoomFacility.php
+++ php/modRoomFacility.php
@@ -11,7 +11,7 @@ $result1=mysql_query("DELETE FROM facility WHERE R_ID=$R_ID");
 #将前台数据全部添加到数据库
 foreach ($data as $key => $value) {
          foreach ($value as $key => $value1) {
-         	$result2=mysql_query("INSERT INTO facility(F_ID,F_Name,F_Num,R_ID) VALUES($value1[F_ID],'$value1[F_Name]',$value1[F_Num],$R_ID)");
+         	$result2=mysql_query("INSERT INTO facility(F_Name,F_Num,R_ID) VALUES('$value1[F_Name]',$value1[F_Num],$R_ID)");
 }
          }
 	
